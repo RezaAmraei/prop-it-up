b13fb8c629f0804d68d7b49585d558e268741446
Reza Amraei <amraeireza@gmail.com>
2022-05-04 22:09:29 -0700

    Putting it Together

--- src/components/NewComponent.js
+++ src/components/NewComponent.js
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 
 class NewComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: this.props.age,
+    };
+  }
+  birthday() {
+    console.log(this.state);
+    let newAge = this.state.age + 1;
+    this.setState({ age: newAge });
+  }
   render() {
     return (
       <div>
         <h1>
           {this.props.lastName}, {this.props.firstName}
         </h1>
-        <p>Age : {this.props.age}</p>
+        {console.log(this.state.age)}
+        <p>Age : {this.state.age}</p>
         <p>Hair Color : {this.props.hairColor} </p>
+        <button onClick={this.birthday.bind(this)}>
+          Birthday button for {this.props.firstName} {this.props.lastName}
+        </button>
       </div>
     );
   }
